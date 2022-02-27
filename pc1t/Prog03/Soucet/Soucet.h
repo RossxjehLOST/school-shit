2353c11047150ce8b7d629bcccfe3731d86c0982
rssxjh lost <100047162+RossxjehLOST@users.noreply.github.com>
2022-02-27 16:58:24 +0100

    Delete pc1t/Prog03 directory

--- pc1t/Prog03/Soucet/Soucet.h
+++
@@ -1,9 +0,0 @@
-﻿// Soucet.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
-
-#include <iostream>
-#include <complex>
-
-// TODO: Reference additional headers your program requires here.
